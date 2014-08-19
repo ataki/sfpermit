94d0e4a34d51da4a5638ce60d7afab1238b1c6c9
Jim <jzheng@salesforce.com>
2014-08-19 00:43:38 -0700

    Add DB stuff

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 ####################
 # Dev Configuration
 ####################
+import os
 
 DEBUG = True
 SECRET_KEY = '1234' # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/bayesimpact'
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/bayesimpact'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 LESS_CONFIG = {
   "SRC": "static/less",
