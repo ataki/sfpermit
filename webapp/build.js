b6e4683acf12a3c52d0e2574f2ef5f7aec9fd9c3
Jim <jzheng@salesforce.com>
2014-09-11 02:43:01 -0700

    Fix static assets

--- webapp/build.js
+++ webapp/build.js
@@ -6,12 +6,15 @@
     out: "dist/app.build.js",
     wrap: true,
     paths: {
-        config: 'config/config',
+        config: 'config',
         jquery: 'bower_components/jquery/dist/jquery.min',
         backbone: 'bower_components/backbone/backbone',
         underscore: 'bower_components/underscore/underscore',
         mustache: 'bower_components/mustache/mustache',
+        moment: 'bower_components/moment/min/moment.min',
+        bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min',
         leaflet: 'bower_components/leaflet/dist/leaflet',
-        leaflet_markercluster: 'bower_components/leaflet.markercluster/dist/leaflet.markercluster'
+        leaflet_markercluster: 'bower_components/leaflet.markercluster/dist/leaflet.markercluster',
+        typeahead: 'bower_components/typeahead.js/dist/typeahead.bundle.min' 
     },
 })
