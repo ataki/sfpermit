e749c993c3ed5ae0182d798462a9f0440e6ce327
Jim <jim32990@gmail.com>
2014-10-01 01:15:45 -0700

    Update db for heroku

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 DEBUG = True
 SECRET_KEY = '1234' # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/bayesimpact'
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://localhost:''@localhost/bayesimpact'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 LESS_CONFIG = {
   "SRC": "static/less",
